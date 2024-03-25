255a1441fb1b11921fe4f79e61ccc1a7c83fe4b8
Your Name <you@example.com>
2024-03-25 19:58:51 +0700

    laravel

---
+++ resources/js/Components/app/SearchForm.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="w-[600px] h-[80px] flex items-center">
+        <TextInput
+            type="text"
+            class="block w-full mr-2"
+            v-model="search"
+            placeholder="Search for files and folders"
+        />
+    </div>
+</template>
+<script setup>
+import { onMounted, ref } from "vue";
+const search = ref("");
+import TextInput from "@/Components/TextInput.vue";
+</script>
---
+++ resources/js/Pages/Profile/MyFiles.vue
@@ -0,0 +1,6 @@
+<template>
+    <AuthenticatedLayout> jjj </AuthenticatedLayout>
+</template>
+<script setup>
+import AuthenticatedLayout from "@/Layouts/AuthenticatedLayout.vue";
+</script>
